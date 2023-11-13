e6850b56fe0201cdb6bcf162b6a4b8de2f42dfe7
heinertill <94447935+heinertill@users.noreply.github.com>
2023-11-13 10:50:58 +0100

    adds h2

---
+++ h2.hs
@@ -0,0 +1,36 @@
+-- nummer 1
+
+productList x = foldr (*) 1 x
+
+-- nummer 2
+
+boolFold x = foldr (&&) True x
+
+-- nummer 3
+
+filter' b (x:[]) = [b x]
+filter' b (x:xs) = b x : filter' b xs
+
+-- nummer 4
+
+dec2nat x = foldl (\acc x -> acc*10 + x) 0 x
+
+-- nummer 5
+
+cubicZ = [x*x*x | x <- [1..]]
+-- primZ = [x | x <- [1..], y <- [2..x], ]
+evenodd = [(x,y) | x <- [0,2..], y <- [1,3..]]
+
+-- nummer 6
+
+split [] = ([], [])
+split [x] = ([x], [])
+split (x:y:xs) = let (left, right) = split xs in (x:left, y:right) 
+
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) = if x <= y then x : merge xs (y:ys) else y : merge (x:xs) ys
+
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = let (left, right) = split xs in merge (mergeSort left) (mergeSort right)
